cbbd8bcac7e7f50e74e277a03518ddb56b62c146
Vitalik <vitalik.yatsenko@jetup.team>
2022-05-10 16:35:39 +0300

    FIX | typing clear

--- types/index.d.ts
+++ types/index.d.ts
@@ -3,33 +3,14 @@ declare module 'validatorjs/src/lang/en' {
 	export default any
 }
 
-declare module 'cachios' {
-	var any: any
-	export default any
-
-	export const CachiosInstance: any
+declare module '*.jpg' {
+	import { ImageSourcePropType } from 'react-native'
+	const value: ImageSourcePropType
+	export default value
 }
 
-declare module 'react-native-expire-storage' {
-	export function setItem(
-		key: string,
-		value: unknown,
-		ttl: number,
-	): Promise<void>
-	export function getItem(key: string): Promise<unknown>
-	export function removeItem(key: string): Promise<void>
-}
-
-declare module 'react-native-image-slider-box' {
-	var any: {
-		SliderBox: any
-	}
-	export default any
-
-	export const SliderBox: any
-}
-
-declare module 'react-native-video-player' {
-	var any: any
-	export default any
+declare module '*.png' {
+	import { ImageSourcePropType } from 'react-native'
+	const value: ImageSourcePropType
+	export default value
 }
